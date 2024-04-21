61eb03462d3950015cbb034538a817cdac796266
Gabriel Samoylov <gsamo122@gmail.com>
2024-04-21 23:01:26 +0300

    ending part 3

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,31 @@
 import express from "express";
+import morgan from "morgan";
+
+import config from "./config";
+import routes from "./routes";
+
+import {
+  globalErrorHandler,
+  notFoundHandler,
+} from "./middlewares/error-handles";
 
 const app = express();
 
+// status checkpoints
+app.get("/status", (req, res) => res.sendStatus(200));
+app.head("/status", (req, res) => res.sendStatus(200));
+
 app.use(express.static("public"));
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(routes);
+app.use(notFoundHandler);
+app.use(globalErrorHandler);
 
-app.get("/hello", (req, res) => res.json("world!!!!!"));
+app.listen(config.app.port, () => {
+  console.log(`http://localhost:${config.app.port}`);
+});
 
-app.listen(3000, () => console.log("http://localhost:3000"));
+// testing
+// import { v4 as uuid } from "uuid";
+// console.log(uuid());
